9fd5f119fb1fc59d3b746efd2e63bc3c5fda0ed6
Glen Fannin <devglen@icloud.com>
2015-05-02 19:16:31 -0400

    updates to client

--- client.c
+++ client.c
@@ -15,71 +15,146 @@
 #include	<netdb.h>
 #include 	<unistd.h>
 #include 	<arpa/inet.h>
+#include 	<fcntl.h>
+#include 	"c_util.h"
+
+#define MAX_BUF_LENGTH 4096
 
 int main(int argc, char *argv[]) {
-	int	socket_fd, send_len, recv_len, from_size;
+	int	socket_fd, send_len, recv_len;
+	//int from_size;
 	struct sockaddr_in	dest, from_serv;
 	struct hostent *gethostbyname(), *hostptr;
+	socklen_t fsize = sizeof(from_serv);
 
-	struct {
-		char cmd[50];
-		char data[512];
-		u_long sector_number;
-	} message_buf;
+	
+	// where socketfd is the socket you want to make non-blocking
+	//int status = fcntl(socket_fd, F_SETFL, fcntl(socket_fd, F_GETFL, 0) | O_NONBLOCK);
 
-	socket_fd = socket (AF_INET, SOCK_DGRAM, 0);
-	if (socket_fd == -1) {
-		perror ("send_udp:socket");
-		exit(1);
-	}
+	//if (status == -1){
+	  //perror("calling fcntl");
+	  // handle the error.  By the way, I've never seen fcntl fail in this way
+	//}
 
-	memset(&dest, 0, sizeof(dest));                /* zero the struct */
-	if ((hostptr = gethostbyname(argv[1])) == NULL){
-		fprintf(stderr, "send_udp: invalid host name, %s\n",argv[1]);
-		exit(1);
-	}
+//	fprintf( stdout, "RECV_UDP Local socket is: \n");
+//	printf ("  family %d, addr %s, port %d\n", dest.sin_family,
+//			inet_ntoa(dest.sin_addr), ntohs((unsigned short)(dest.sin_port)));
+//	fflush(stdout);
 
-	dest.sin_family = AF_INET;
-	bcopy(hostptr->h_addr, (char *)&dest.sin_addr,hostptr->h_length);
-	dest.sin_port = htons((u_short)0x3333);
-
-	fprintf( stdout, "RECV_UDP Local socket is: \n");
-	printf ("  family %d, addr %s, port %d\n", dest.sin_family,
-			inet_ntoa(dest.sin_addr), ntohs((unsigned short)(dest.sin_port)));
-	fflush(stdout);
-
-	char kill[5] = "DDDD\0";
 	while ( 1 ) {
-		char x[50];
-		printf("What would you like to do? ");
-		scanf("%s",x);
-		printf("sent this to server: %s\n", x);
+		memset(&message_buf, 0, sizeof(message_buf));  
+		fprintf(stdout, "flop: ");
 
+		char input[50], command[50], arg[50];
+		fgets(input, 50, stdin); //read line
+		sscanf(input, "%s", command); //separate out first arg, consider using strtok() instead of sscanf()
 
-		if(strcmp(kill, x) == 0){
-		   fprintf(stdout, "entered: %s\n", x);
-		   close(socket_fd);
-		   break;
+		if (strcmp("quit", command) == 0) {
+			fprintf(stdout, "\n Exiting the floppy disk shell... \n");
+			close(socket_fd);
+			return EXIT_SUCCESS;
+		} else if (strcmp("help", command) == 0) {
+			help();
+			continue;
+		} else if (strcmp("fmount", command) == 0) {
+			sscanf(input, "%s %s", command, arg);
+			
+			socket_fd = socket (AF_INET, SOCK_DGRAM, 0);
+			if (socket_fd == -1) {
+				perror ("send_udp:socket");
+				exit(1);
+			}
+
+			memset(&dest, 0, sizeof(dest));                /* zero the struct */
+			if ((hostptr = gethostbyname(arg)) == NULL){
+				fprintf(stderr, "send_udp: invalid host name, %s\n",argv[1]);
+				exit(1);
+			}
+
+			dest.sin_family = AF_INET;
+			bcopy(hostptr->h_addr, (char *)&dest.sin_addr,hostptr->h_length);
+			dest.sin_port = htons((u_short)0x3333);
+
+			strcpy(message_buf.cmd, command);
+			
+			send_len = sendto(socket_fd, &message_buf, sizeof(message_buf), 0, (struct sockaddr *)&dest, sizeof(dest));
+			if (send_len < 0) {
+				perror("There was an error, please make sure the floppy is mounted. ");
+				exit(1);
+			}
+		} else if (strcmp("fumount", command) == 0) {
+			sscanf(input, "%s %s", command, arg);
+			
+			memset(&dest, 0, sizeof(dest));                /* zero the struct */
+			if ((hostptr = gethostbyname(arg)) == NULL){
+				fprintf(stderr, "send_udp: invalid host name, %s\n",argv[1]);
+				exit(1);
+			}
+
+			dest.sin_family = AF_INET;
+			bcopy(hostptr->h_addr, (char *)&dest.sin_addr,hostptr->h_length);
+			dest.sin_port = htons((u_short)0x3333);
+			
+			strcpy(message_buf.cmd, command);
+			
+			send_len = sendto(socket_fd, &message_buf, sizeof(message_buf), 0, (struct sockaddr *)&dest, sizeof(dest));
+			if (send_len < 0) {
+				perror("There was an error, please make sure the floppy is mounted. ");
+				exit(1);
+			}
+		} else if (strcmp("showsector", command) == 0) {
+			sscanf(input, "%s %s", command, arg);
+			
+			message_buf.sector_number = atoi(arg);
+			strcpy(message_buf.cmd, command);
+			
+			send_len = sendto(socket_fd, &message_buf, sizeof(message_buf), 0, (struct sockaddr *)&dest, sizeof(dest));
+			if (send_len < 0) {
+				perror("There was an error, please make sure the floppy is mounted. ");
+				exit(1);
+			}
+		} else if (strcmp("structure", command) == 0) {
+			structure();
+		} else if(strcmp("traverse", command) == 0) {
+			if (strstr(input, "-l") == NULL) {
+				traverse(command);
+			} else if (strstr(input, "-l") != NULL) {
+				sscanf(input, "%s %s", command, arg);
+				traverse(arg);
+			}
+			else {
+				printf("Error invalid argument, please try again! \n");
+				continue;
+			}
+		} else {
+			fprintf(stdout, "Invalid command, please try again.\n");
+			continue;
 		}
+		
+		memset(&message_buf, 0, sizeof(message_buf));
 
-		strcpy(message_buf.cmd, x);
-		message_buf.sector_number = htonl(10);
-		strcpy(message_buf.data, "<>");
+		recv_len = recvfrom(socket_fd, &message_buf, sizeof(message_buf), 0, (struct sockaddr *)&from_serv, &fsize);
 
-		send_len = sendto(socket_fd, &message_buf, sizeof(message_buf), 0, (struct sockaddr *)&dest, sizeof(dest));
-		if (send_len < 0) {
-			perror("send_udp:sendto");
-			exit(1);
+		/*do {
+			recv_len = recvfrom(socket_fd, &message_buf, sizeof(message_buf), 0, (struct sockaddr *)&from_serv, &fsize);
+
+			if (recv_len == -1) {
+				fprintf(stdout, "There was an error");
+			} else {
+				rcv_data += message_buf.data;
+			}
+		} while ( recv_len >= MAX_BUF_LENGTH );*/
+
+		if (strcmp("fmount", command) == 0) {
+			fprintf(stdout, "%s\n", message_buf.data);
+		} else if (strcmp("fumount", command) == 0) {
+			close(socket_fd);
+			fprintf(stdout, "%s\n", message_buf.data);
+		} else if (strcmp("showsector", command) == 0) {
+			showsector();
+			fflush(stdout);
 		}
-
-		printf("After send\n");
-
-		from_size = sizeof(from_serv);
-		recv_len = recv(socket_fd, &message_buf, sizeof(message_buf), 0);
-
-		fprintf(stdout, "Received new data: %s, (%d bytes).\n", message_buf.data, recv_len);
 	}
 
 	return 0;
-
 }
