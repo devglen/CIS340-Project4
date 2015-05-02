aec8ce44635121ddf6895097a7a03c7d0ebd62c0
Glen Fannin <devglen@icloud.com>
2015-05-02 19:16:54 -0400

    updates to server

--- server.c
+++ server.c
@@ -4,6 +4,7 @@
  *  Created on: Apr 28, 2015
  *      Author: Glen
  */
+
 #include	<signal.h>
 #include	<errno.h>
 #include	<strings.h>
@@ -14,12 +15,22 @@
 #include	<netinet/in.h>
 #include	<netdb.h>
 #include 	<arpa/inet.h>
-#include 	"util.h"
+#include    	<fcntl.h>
+#include 	"utility.h"
+
+#define 	MAX_DATA_LENGTH 512
+
 
 int main(int argc, char *argv[]) {
-	struct sockaddr_in dest; /* socket info about the machine connecting to us */
+	// mount the floppy on execution
+	if (argc > 1) {
+		fmount(argv[1]);
+	} else {
+		fprintf(stdout, "Please provide an imagefile to mount.\n");
+		exit(1);
+	}
 	struct sockaddr_in serv, from_serv; /* socket info about our server */
-	int socket_fd, from_size, recv_len;            /* socket used to listen for incoming connections */
+	int socket_fd, recv_len;            /* socket used to listen for incoming connections */
 	socklen_t socksize = sizeof(struct sockaddr_in);
 
 	memset(&serv, 0, sizeof(serv));           /* zero the struct before filling the fields */
@@ -46,31 +57,38 @@ int main(int argc, char *argv[]) {
 
 	int consocket = accept(socket_fd, (struct sockaddr *)&from_serv, &socksize);
 
-	//fprintf(stdout, "Waiting for Connection Request... \n");
-	char kill[5] = "KKKK\0";
 	while( 1 ) {
+		memset(&message, 0, sizeof(message));
 		recv_len = recvfrom(socket_fd, &message, sizeof(message), 0, (struct sockaddr *)&from_serv, &socksize);
 		if (recv_len < 0)
 			perror("recv_udp:recvfrom");
-		fprintf(stdout, "recv_udp: %d, Packet from:", &from_serv);
-		fprintf(stdout, "Got data ::%s%d%c\n",message.cmd,ntohl(message.data),ntohl(message.sector_number));
-		fflush(stdout);
-
-		if(strcmp(kill, message.cmd) == 0){
-			printf("entered the kill word: %s\n", message.cmd);
-			close(consocket);
-			close(socket_fd);
-			return EXIT_SUCCESS;
-		}
+//		fprintf(stdout, "recv_udp: %d, Packet from:", &from_serv);
+//		fprintf(stdout, "Got data ::%s%s\n",message.cmd,message.data);
+//		fflush(stdout);
 
 		if(recv_len > 0){
-			printf("\n RECIEVED DATA = %lu \n" , sizeof(message));
-			printf("Command received is: %s\n", message.cmd);
-			printf("Incoming connection from %s - sending reversed message\n", inet_ntoa(from_serv.sin_addr));
+			printf("Incoming connection from %s \n", inet_ntoa(from_serv.sin_addr));
 
-			strcpy(message.data, "<NEW DATA>");
-
-			sendto(socket_fd, &message, sizeof(message), 0, (struct sockaddr *)&from_serv, sizeof(from_serv));
+			if (strcmp("fmount", message.cmd) == 0) {
+				if (fd > -1) {
+					strcpy(message.data, "Floppy mounted successfully.");
+					message.size = sizeof("Floppy mounted successfully.");
+				} else {
+					strcpy(message.data, "There was a problem mounting the floppy.");
+					message.size = sizeof("There was a problem mounting the floppy.");
+				}
+				sendto(socket_fd, &message, sizeof(message), 0, (struct sockaddr *)&from_serv, sizeof(from_serv));
+			} else if (strcmp("fumount", message.cmd) == 0) {
+				strcpy(message.data, "Floppy has been unmounted successfully.");
+				message.size = sizeof("Floppy has been unmounted successfully.");
+				sendto(socket_fd, &message, sizeof(message), 0, (struct sockaddr *)&from_serv, sizeof(from_serv));
+			} else {
+				get_sector_data(message.sector_number);
+				memcpy(message.data, buf, message.size);
+				
+				sendto(socket_fd, &message, sizeof(message), 0, (struct sockaddr *)&from_serv, sizeof(from_serv));
+			}
+			
 		} else {
 			consocket = accept(socket_fd, (struct sockaddr *)&from_serv, &socksize);
 		}
