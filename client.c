6c439872e5ffd70440ec151aa8c8c34069d0a878
Glen Fannin <devglen@icloud.com>
2015-05-02 22:49:34 -0400

    fixes to structure method

--- client.c
+++ client.c
@@ -27,7 +27,7 @@ int main(int argc, char *argv[]) {
 	struct hostent *gethostbyname(), *hostptr;
 	socklen_t fsize = sizeof(from_serv);
 
-	
+
 	// where socketfd is the socket you want to make non-blocking
 	//int status = fcntl(socket_fd, F_SETFL, fcntl(socket_fd, F_GETFL, 0) | O_NONBLOCK);
 
@@ -42,7 +42,7 @@ int main(int argc, char *argv[]) {
 //	fflush(stdout);
 
 	while ( 1 ) {
-		memset(&message_buf, 0, sizeof(message_buf));  
+		memset(&message_buf, 0, sizeof(message_buf));
 		fprintf(stdout, "flop: ");
 
 		char input[50], command[50], arg[50];
@@ -58,7 +58,7 @@ int main(int argc, char *argv[]) {
 			continue;
 		} else if (strcmp("fmount", command) == 0) {
 			sscanf(input, "%s %s", command, arg);
-			
+
 			socket_fd = socket (AF_INET, SOCK_DGRAM, 0);
 			if (socket_fd == -1) {
 				perror ("send_udp:socket");
@@ -76,7 +76,7 @@ int main(int argc, char *argv[]) {
 			dest.sin_port = htons((u_short)0x3333);
 
 			strcpy(message_buf.cmd, command);
-			
+
 			send_len = sendto(socket_fd, &message_buf, sizeof(message_buf), 0, (struct sockaddr *)&dest, sizeof(dest));
 			if (send_len < 0) {
 				perror("There was an error, please make sure the floppy is mounted. ");
@@ -84,7 +84,7 @@ int main(int argc, char *argv[]) {
 			}
 		} else if (strcmp("fumount", command) == 0) {
 			sscanf(input, "%s %s", command, arg);
-			
+
 			memset(&dest, 0, sizeof(dest));                /* zero the struct */
 			if ((hostptr = gethostbyname(arg)) == NULL){
 				fprintf(stderr, "send_udp: invalid host name, %s\n",argv[1]);
@@ -94,9 +94,9 @@ int main(int argc, char *argv[]) {
 			dest.sin_family = AF_INET;
 			bcopy(hostptr->h_addr, (char *)&dest.sin_addr,hostptr->h_length);
 			dest.sin_port = htons((u_short)0x3333);
-			
+
 			strcpy(message_buf.cmd, command);
-			
+
 			send_len = sendto(socket_fd, &message_buf, sizeof(message_buf), 0, (struct sockaddr *)&dest, sizeof(dest));
 			if (send_len < 0) {
 				perror("There was an error, please make sure the floppy is mounted. ");
@@ -104,17 +104,26 @@ int main(int argc, char *argv[]) {
 			}
 		} else if (strcmp("showsector", command) == 0) {
 			sscanf(input, "%s %s", command, arg);
-			
+
 			message_buf.sector_number = atoi(arg);
 			strcpy(message_buf.cmd, command);
-			
+
 			send_len = sendto(socket_fd, &message_buf, sizeof(message_buf), 0, (struct sockaddr *)&dest, sizeof(dest));
 			if (send_len < 0) {
 				perror("There was an error, please make sure the floppy is mounted. ");
 				exit(1);
 			}
 		} else if (strcmp("structure", command) == 0) {
-			structure();
+			sscanf(input, "%s %s", command, arg);
+
+			message_buf.sector_number = 0;
+			strcpy(message_buf.cmd, command);
+
+			send_len = sendto(socket_fd, &message_buf, sizeof(message_buf), 0, (struct sockaddr *)&dest, sizeof(dest));
+			if (send_len < 0) {
+				perror("There was an error, please make sure the floppy is mounted. ");
+				exit(1);
+			}
 		} else if(strcmp("traverse", command) == 0) {
 			if (strstr(input, "-l") == NULL) {
 				traverse(command);
@@ -130,7 +139,7 @@ int main(int argc, char *argv[]) {
 			fprintf(stdout, "Invalid command, please try again.\n");
 			continue;
 		}
-		
+
 		memset(&message_buf, 0, sizeof(message_buf));
 
 		recv_len = recvfrom(socket_fd, &message_buf, sizeof(message_buf), 0, (struct sockaddr *)&from_serv, &fsize);
@@ -153,6 +162,23 @@ int main(int argc, char *argv[]) {
 		} else if (strcmp("showsector", command) == 0) {
 			showsector();
 			fflush(stdout);
+		} else if (strcmp("structure", command) == 0) {
+			structure();
+			fflush(stdout);
+		} else if(strcmp("traverse", command) == 0) {
+			if (strstr(input, "-l") == NULL) {
+				traverse(command);
+				fflush(stdout);
+			} else if (strstr(input, "-l") != NULL) {
+				sscanf(input, "%s %s", command, arg);
+				traverse(arg);
+				fflush(stdout);
+			}
+			else {
+				printf("Error invalid argument, please try again! \n");
+				fflush(stdout);
+				continue;
+			}
 		}
 	}
 
